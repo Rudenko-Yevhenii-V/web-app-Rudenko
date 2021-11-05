26a6411343d0e61b847dc2c8c643c4124fd6c0f9
LinuxRTTR <rydui403@mail.com>
2021-11-05 19:09:03 +0200

    add test ThemeControllerTest.java

---
+++ english-lessons-web-app/src/test/java/ry/rudenko/englishlessonswebapp/controller/ThemeControllerTest.java
@@ -0,0 +1,76 @@
+package ry.rudenko.englishlessonswebapp.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ry.rudenko.englishlessonswebapp.Routes;
+import ry.rudenko.englishlessonswebapp.model.dto.ThemeDto;
+
+@Log4j2
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+class ThemeControllerTest {
+
+  @Autowired
+   MockMvc mvc;
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Test
+  void fetchThemes() throws Exception {
+    String themeName = "Timestest";
+    MvcResult result = mvc
+        .perform(
+            MockMvcRequestBuilders.post(
+                "/api/v1/themes/{themeName}".replace("{themeName}", themeName)
+            )
+        )
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andReturn();
+    ThemeDto dto = objectMapper.readValue(result.getResponse().getContentAsString(), ThemeDto.class);
+    assert (dto.getName().equals(themeName));
+    log.info( result.getResponse().toString());
+  }
+
+  @Test
+  void createTheme() {
+  }
+
+  @Test
+  void deleteTheme() {
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- english-lessons-web-app/src/test/java/ry/rudenko/englishlessonswebapp/controller/TodoControllerTest.java
+++ english-lessons-web-app/src/test/java/ry/rudenko/englishlessonswebapp/controller/TodoControllerTest.java
@@ -2,7 +2,24 @@ package ry.rudenko.englishlessonswebapp.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class TodoControllerTest {
 
+  @Test
+  void createTodo() {
+
+  }
+
+  @Test
+  void completeTodo() {
+  }
 }
