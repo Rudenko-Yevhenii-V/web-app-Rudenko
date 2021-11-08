4ad191935e4ca02ea6dad6e1a84fb82270ec2001
LinuxRTTR <rydui403@mail.com>
2021-11-08 12:51:23 +0200

    return to main

--- english-lessons-web-app/src/main/java/ry/rudenko/englishlessonswebapp/config/security/SecurityConfig.java
+++ english-lessons-web-app/src/main/java/ry/rudenko/englishlessonswebapp/config/security/SecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 //@EnableConfigurationProperties(FileSharingSecurityProperties.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+  private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
 //    private final FileSharingSecurityProperties securityProperties;
 //
@@ -63,47 +63,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
 //    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // open static resources
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                // open swagger-ui
-                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"
-                , "/users*", "/**").permitAll()
-                // allow user registration and refresh, ignore authorization filters on login
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        // open static resources
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        // open swagger-ui
+        .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"
+            , "/users*", "/**").permitAll()
+        // allow user registration and refresh, ignore authorization filters on login
 //                .antMatchers(HttpMethod.POST, Routes.USERS, Routes.TOKEN + "/refresh").permitAll()
-                // admin can register new admins
+        // admin can register new admins
 //                .antMatchers(HttpMethod.POST, Routes.USERS + "/admins").hasRole("ADMIN")
-                // regular users can view basic user info for other users
+        // regular users can view basic user info for other users
 //                .antMatchers(HttpMethod.GET,Routes.USERS + "/{id:\\d+}").authenticated()
-                // admin can manage users by id
+        // admin can manage users by id
 //                .antMatchers(Routes.USERS + "/{id:\\d+}/**").hasRole("ADMIN")
-                // admin can use Actuator endpoints
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
-                // by default, require authentication
-                .anyRequest().authenticated()
-                .and()
-                // auth filter
+        // admin can use Actuator endpoints
+        .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+        // by default, require authentication
+        .anyRequest().authenticated()
+        .and()
+        // auth filter
 //                .addFilter(jwtAuthenticationFilter())
-                // jwt-verification filter
+        // jwt-verification filter
 //                .addFilter(jwtAuthorizationFilter())
-                // for unauthorized requests return 401
-                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .and()
-                // allow cross-origin requests for all endpoints
-                .cors().configurationSource(corsConfigurationSource())
-                .and()
-                .csrf().disable()
-                // this disables session creation on Spring Security
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+        // for unauthorized requests return 401
+        .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        .and()
+        // allow cross-origin requests for all endpoints
+        .cors().configurationSource(corsConfigurationSource())
+        .and()
+        .csrf().disable()
+        // this disables session creation on Spring Security
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
 
 
 
-    private CorsConfigurationSource corsConfigurationSource() {
-        var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
+  private CorsConfigurationSource corsConfigurationSource() {
+    var source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    return source;
+  }
 }
